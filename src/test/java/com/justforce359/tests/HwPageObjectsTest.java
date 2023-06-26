7763361405895e2e1030c05951031a1b0ad10c97
Mikhail Ustinov <mikhailu359@gmail.com>
2023-06-26 14:50:57 +0300

    added static testdata

--- src/test/java/com/justforce359/tests/HwPageObjectsTest.java
+++ src/test/java/com/justforce359/tests/HwPageObjectsTest.java
@@ -2,19 +2,21 @@ package com.justforce359.tests;
 
 
 import com.justforce359.pages.RegistrationPage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static com.justforce359.tests.TestData.*;
 
 
 public class HwPageObjectsTest extends TestBase {
-
     RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
     void fillFormTest() {
         registrationPage.openPage()
-                .setFirstname("Mikhail")
-                .setLastname("Ustinov")
-                .setUserEmail("dsvsdv@mail.com")
+                .setFirstname(firstName)
+                .setLastname(lastName)
+                .setUserEmail(userEmail)
                 .setGender("Male")
                 .userNumber("1234567890")
                 .setBirthDate("10", "May", "2000")
@@ -26,9 +28,9 @@ public class HwPageObjectsTest extends TestBase {
                 .submitClick();
 
         registrationPage.shouldHave("Thanks for submitting the form")
-                .resultFormCheck("Student Name", "Mikhail Ustinov")
-                .resultFormCheck("Student Email", "dsvsdv@mail.com")
-                .resultFormCheck("Student Email", "dsvsdv@mail.com")
+                .resultFormCheck("Student Name", firstName
+                        + " " + lastName)
+                .resultFormCheck("Student Email", userEmail)
                 .resultFormCheck("Gender", "Male")
                 .resultFormCheck("Mobile", "1234567890")
                 .resultFormCheck("Date of Birth", "10 May,2000")
---
+++ src/test/java/com/justforce359/tests/TestData.java
@@ -0,0 +1,8 @@
+package com.justforce359.tests;
+
+public class TestData {
+
+    public static String firstName = "Mikhail",
+            lastName = "Ustinov",
+            userEmail = "dsvsdv@mail.com";
+}
